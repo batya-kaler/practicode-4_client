1f010cab12ea9e05549375df6d7597845b000468
Batya <batyakaler@gmail.com>
2025-03-12 16:49:11 +0200

    update a service

--- src/service.js
+++ src/service.js
@@ -1,26 +1,34 @@
 import axios from'./axiosConfig.ts'
 axios.defaults.baseURL = process.env.NODE_ENV;
-const service = {
+
+axios.interceptors.response.use(
+  (response) => response, // אם אין שגיאות, מחזיר את התגובה כרגיל
+  (error) => {
+    console.error("Axios Error:", error.response ? error.response.data : error.message);
+    return Promise.reject(error);
+  }
+);
+
+export default {
   getTasks: async () => {
     const result = await axios.get(`/items`);
     return result.data;
   },
- 
   addTask: async (name) => {
     console.log('addTask', name);
-    const result = await axios.post(`/`, { name }); // אם יש צורך בגוף הבקשה
+    const result = await axios.post(`/`, { name })
     return result.data;
   }, 
 
   setCompleted: async (id, isComplete) => {
     console.log('setCompleted', { id, isComplete });
-    const result = await axios.patch(`/${id}`, { IsComplete: isComplete }); // אם יש צורך בגוף הבקשה
+    const result = await axios.patch(`/${id}`, { IsComplete: isComplete })
     return result.data;
   },
 
   deleteTask: async (id) => {
     console.log('deleteTask', id);
-    const result = await axios.delete(`/${id}`); // ודא שה-URL נכון
+    const result = await axios.delete(`/${id}`)
     return result.data;
   }
 };

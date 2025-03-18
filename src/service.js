3211121af93d06e8c339f7d748b5863d5306afdf
Batya <batyakaler@gmail.com>
2025-03-18 13:43:55 +0200

    update a service

--- src/service.js
+++ src/service.js
@@ -1,3 +1,31 @@
+// import axios from 'axios';
+// axios.defaults.baseURL = process.env.NODE_ENV;
+// export default {
+//   getTasks: async () => {
+//     const result = await axios.get(`/items`);
+//     return result.data.Items;
+//   },
+//   addTask: async (name) => {
+//     console.log('addTask', name);
+//     const result = await axios.post(`/`, { name })
+//     return result.data;
+//   }, 
+
+//   setCompleted: async (id, isComplete) => {
+//     console.log('setCompleted', { id, isComplete });
+//     const result = await axios.patch(`/${id}`, { IsComplete: isComplete })
+//     return result.data;
+//   },
+
+//   deleteTask: async (id) => {
+//     console.log('deleteTask', id);
+//     const result = await axios.delete(`/${id}`)
+//     return result.data;
+//   }
+// };
+
+
+
 import axios from 'axios';
 axios.defaults.baseURL = process.env.NODE_ENV;
 export default {
@@ -7,19 +35,19 @@ export default {
   },
   addTask: async (name) => {
     console.log('addTask', name);
-    const result = await axios.post(`/`, { name })
+    const result = await axios.post(`/`, { name });
     return result.data;
   }, 
 
   setCompleted: async (id, isComplete) => {
     console.log('setCompleted', { id, isComplete });
-    const result = await axios.patch(`/${id}`, { IsComplete: isComplete })
+    const result = await axios.patch(`/${id}`, { IsComplete: isComplete });
     return result.data;
   },
 
   deleteTask: async (id) => {
     console.log('deleteTask', id);
-    const result = await axios.delete(`/${id}`)
+    const result = await axios.delete(`/${id}`);
     return result.data;
   }
 };
